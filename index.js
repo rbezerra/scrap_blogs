e843ba708f8bbd67707270c18c2e7b2924c8313a
Ramon Bezerra <ramonbezerra90@gmail.com>
2019-07-31 22:07:21 -0300

    mudando para a biblioteca axios

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-const rp = require('request-promise');
-const $ = require('cheerio');
-const url = 'https://luiscardoso.com.br/?s=feminicidio';
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
 
-rp(url)
-	.then(html => {
-		const articles = $('article', html);
+const url = "https://luiscardoso.com.br/?s=feminicidio";
+const outputFile = "posts.json";
 
-		const keys = Object.keys(articles);
+const getWebContent = async url => {
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
 
-        for (const key of keys){
-            const post_title = $('h1.post-title > a', articles[key].children);
-            console.log(post_title.text());
-        }
-	})
-	.catch(err => console.error(err));
+    extracted_links = [];
+
+    $("article").each((i, article) => {
+      const post_title = $("h1.post-title > a", article).text();
+      const post_link = $("h1.post-title > a", article).attr("href");
+
+      if (post_title && post_link) {
+        const newArticle = { post_title, post_link };
+
+        extracted_links.push(newArticle);
+      }
+	});
+	
+	exportResults(extracted_links);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const exportResults = parsedResults => {
+  fs.writeFile(outputFile, JSON.stringify(parsedResults, null, 4), err => {
+    if (err) console.error(err);
+
+    console.log(`Resultados exportados com sucesso para ${outputFile}`);
+  });
+};
+
+getWebContent(url);
